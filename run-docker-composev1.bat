a0527cc26c0cb3178b94ea61f2c330f2b400a878
El Chorri <desarrollador_sme03@minedu.gob.pe>
2020-10-21 08:52:55 -0500

    Foo

--- run-docker-composev1.bat
+++
@@ -1,7 +0,0 @@
-docker-compose -f .\docker-composev1.yml down
-docker rmi aprendo-en-casa-offline-contenido.api:v1 --force
-docker system prune --force
-docker-compose -f .\docker-composev1.yml  build
-docker network create ms
-docker-compose -f .\docker-composev1.yml  up -d
-pause
