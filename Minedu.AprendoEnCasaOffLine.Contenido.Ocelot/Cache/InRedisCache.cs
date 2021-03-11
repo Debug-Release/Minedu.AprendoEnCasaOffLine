6f274e34fcb809211ad64c247b90c4e47821734f
Antonio <antonio@THINKPAD>
2021-03-11 10:46:59 -0500

    Cambios

---
+++ Minedu.AprendoEnCasaOffLine.Contenido.Ocelot/Cache/InRedisCache.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration;
+using Ocelot.Cache;
+using System;
+
+namespace Minedu.AprendoEnCasaOffLine.Contenido.Ocelot
+{
+    public class RedisConfig 
+    {
+        public RedisConfig()
+        {
+            ConnectionString = new string[] { };
+        }
+        public string[] ConnectionString { get; set; }
+    }
+    public class InRedisCache<T> : IOcelotCache<T>
+    {       
+        public InRedisCache(IConfiguration configuration)
+        {          
+            var redisConfig = new RedisConfig();
+            configuration.GetSection("Redis").Bind(redisConfig);
+
+            var csredis = new CSRedis.CSRedisClient(null, redisConfig.ConnectionString);
+            RedisHelper.Initialization(csredis);
+
+        }
+
+        public void Add(string key, T value, TimeSpan ttl, string region)
+        {
+            key = GetKey(region, key);
+
+            if (ttl.TotalMilliseconds <= 0)
+            {
+                return;
+            }
+            //RedisHelper.Set(key, value.ToJson(), (int)ttl.TotalSeconds);
+            RedisHelper.Set(key, value, (int)ttl.TotalSeconds);
+        }
+
+        public void AddAndDelete(string key, T value, TimeSpan ttl, string region)
+        {
+            Add(key, value, ttl, region);
+        }
+
+        public void ClearRegion(string region)
+        {
+            var data = RedisHelper.Keys(GetKey(region, "*"));
+            RedisHelper.Del(data);
+        }
+
+        public T Get(string key, string region)
+        {
+            key = GetKey(region, key);
+            var result = RedisHelper.Get<T>(key);
+            if (result != null)
+            {
+                return (T)result;
+            }
+            return default(T);
+        }
+
+        private string GetKey(string region, string key)
+        {
+            return $"{region}-{key}".ToLower();
+        }
+    }
+}
--- Minedu.AprendoEnCasaOffLine.Contenido.Ocelot/Startup.cs
+++ Minedu.AprendoEnCasaOffLine.Contenido.Ocelot/Startup.cs
@@ -1,68 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.Cache;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using System;
 
 namespace Minedu.AprendoEnCasaOffLine.Contenido.Ocelot
 {
-    public class InRedisCache<T> : IOcelotCache<T>
-    {
-        private readonly IConfiguration _configuration;
-        public InRedisCache(IConfiguration configuration)
-        {
-            _configuration = configuration;
-            string cs = _configuration.GetSection("Redis:ConnectionString").Value;
-
-            var csredis = new CSRedis.CSRedisClient(cs);
-            RedisHelper.Initialization(csredis);
-
-        }
-
-        public void Add(string key, T value, TimeSpan ttl, string region)
-        {
-            key = GetKey(region, key);
-
-            if (ttl.TotalMilliseconds <= 0)
-            {
-                return;
-            }
-            //RedisHelper.Set(key, value.ToJson(), (int)ttl.TotalSeconds);
-            RedisHelper.Set(key, value, (int)ttl.TotalSeconds);
-        }
-
-        public void AddAndDelete(string key, T value, TimeSpan ttl, string region)
-        {
-            Add(key, value, ttl, region);
-        }
-
-        public void ClearRegion(string region)
-        {
-            var data = RedisHelper.Keys(GetKey(region, "*"));
-            RedisHelper.Del(data);
-        }
-
-        public T Get(string key, string region)
-        {
-            key = GetKey(region, key);
-            var result = RedisHelper.Get<T>(key);
-            if (result != null)
-            {
-                return (T)result;
-            }
-            return default(T);
-        }
-
-        private string GetKey(string region, string key)
-        {
-            return $"{region}-{key}".ToLower();
-        }
-    }
 
     public class Startup
     {
